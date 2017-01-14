31b418ffa8fdcfc7413ca9182d7fd7ca37a5cb90
Josh Humpherys <joshua.humpherys@gmail.com>
2017-01-14 09:26:17 -0500

    Update README.md and add Hello.java
    initial short asg check in

--- README.md
+++ README.md
@@ -1,5 +1,8 @@
 COS375Spring2013ShortAsgSCM
 ===========================
+
+:octocat: **Josh Humpherys** joshhumpherys
+
 Andrew Bikle -- AngleOSaxon
 Will Loderhose - willloderhose
 Rob Willett robwillett
---
+++ joshhumpherys/Hello.java
@@ -0,0 +1,28 @@
+import java.util.Random;
+
+public class Hello {
+    public static void main(String[] args) {
+        StringBuilder sb = new StringBuilder();
+        sb.append(capitalizeString(randomString(-229985452)));
+        sb.append(" ");
+        sb.append(capitalizeString(randomString(-147909649)));
+        System.out.println(sb.toString());
+    }
+
+    private static String randomString(int i) {
+        Random r = new Random(i);
+        StringBuilder sb = new StringBuilder();
+        while(true) {
+            int k = r.nextInt(27);
+            if(k == 0) {
+                break;
+            }
+            sb.append((char)('`' + k));
+        }
+        return sb.toString();
+    }
+
+    private static String capitalizeString(String s) {
+        return s.substring(0, 1).toUpperCase() + s.substring(1);
+    }
+}
