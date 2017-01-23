c722e04167f100940dcdc18956b5d036c8eabedc
Nick Gilbert <nickgilbert1994@gmail.com>
2017-01-23 12:58:04 -0500

    Initial Short Asg Check-In

---
+++ nickgilbert1994/hello.java
@@ -0,0 +1,12 @@
+/**
+ * Nick Gilbert
+ * COS375: Software Engineering
+ *
+ * Hello world file for asg 1
+ */
+ 
+ public class HelloWorld {
+	public static void main(String[] args) {
+		System.out.println("Hello, world!");
+	}
+ }
